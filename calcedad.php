7459a213bfe20c8e4def1e42e31c645473885bed
luis2002801 <71188501+luis2002801@users.noreply.github.com>
2020-10-04 22:08:06 -0500

    momento1

--- calcedad.php
+++ calcedad.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>mejoresjug</title>
+    <title>calculaedad</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" 
     integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
@@ -11,67 +11,52 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 </head>
 <body>
-<header>
-    <nav class="navbar navbar-expand-ms navbar-dark bg-warning">
-            <a class="navbar-brand" href="#">Jugadores Colomboeuropeo</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
+   <header>
+     <nav class="navbar navbar-expand-ms navbar-dark bg-warning">
+              <a class="navbar-brand" href="#">Jugadores Colomboeuropeo</a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+              </button>
                 <form class="form-inline my-2 my-ms-0">
                     <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
                     <button class="btn btn-outline-dark my-2 my-sm-0" type="submit">Search</button>
                 </form>
+          </div>
+      </nav>
+    </header>
+     <div class="container">
+       <h3>Calculando la edad automaticamente</h3>
+         <div class="row">
+            <div class="col-sm-3">
+                <label>escriba la fecha de nacimiento</label>
+                  <input type="text" id="calender" name="calender" class="form-control input-sm" reandoly="">
+                      <span class="label label-success">
+                         <span id="edadcalculada"></span>
+                       </span>
+                  <button type="submit" class="btn btn-info mt-3 btn-block" name="botonCalcular">CALCULAR</button>
+                        <?php
+                        if(isset($_POST["botonCalcular"]))?>
+                        <?php
+                        function Calculandotiempo($fechainicio,$fechafin){
+                        
+                        $datetime1 = date_create($fechainicio);
+                        $datetime2 = date_create($fechafin);
+                        $interval = date_diff($datetime1,$datetime2);
+                        
+                        $tiempo = array();
+                        foreach ($interval as $valor){
+                          $tiempo[]=$valor;
+                        }
+                        return $tiempo;
+                        
+                        $fechainicio= $_POST['fecha'];
+                        $fechafin=date(Y-m-d);
+                        echo Calculandotiempo($fechainicio,$fechafin);
+                        }
+                          ?>
+              
             </div>
-        </nav>
-</header>
-<div class="container">
-<h3>Calculando la edad automaticamente</h3>
-<div class="row">
-    <div class="col-sm-3">
-    <label>escriba la fecha de nacimiento</label>
-    <input type="text" id="calender" name="calender" class="form-control input-sm" reandoly="">
-    <span class="label label-success">
-    <span id="edadcalculada"></span>
-    </span>
-    <button type="submit" class="btn btn-info mt-3 btn-block" name="botonCalcular">CALCULAR</button>
-<?php
-if(isset($_POST["botonCalcular"]))?>
-<?php
-function Calculandotiempo($fechainicio,$fechafin){
-
-$datetime1 = date_create($fechainicio);
-$datetime2 = date_create($fechafin);
-$interval = date_diff($datetime1,$datetime2);
-
-$tiempo = array();
-foreach ($interval as $valor){
-  $tiempo[]=$valor;
-}
-return $tiempo;
-
-$fechainicio= $_POST['fecha'];
-$fechafin=date(Y-m-d);
-echo Calculandotiempo($fechainicio,$fechafin);
-}
- ?>   
+          </div>
+      </div>
 </body>
 </html>
-<script type="text/javascript">
-$(document).ready(function(){
-  $("#calendario").datepicker({
-    changeMonth: true,
-    changeYear: true,
-    yearRanger:'1900:' + 2020,
-    dateFormat: "yy-mm-dd"
-  });
-  $('#calendario').change(function(){
-    $.ajax({
-      type:"post",
-      data:"fecha=" + $('#calendario').val(),
-      success:function(r){
-        $('#edadCalculada'). text(r + "años");
-      }
-    })
-  })
-})
-</script>
--- gimnasio.php
+++ gimnasio.php
@@ -8,8 +8,8 @@
     integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
 <body>
-<header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-success">
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-success">
             <a class="navbar-brand" href="#">Gimnasio Bodytech</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -26,53 +26,56 @@
             <div class="row justify-content-center">
                 <div class="col-4">
 
-                <form class="mt-3" action="gimnasio.php" method="POST">
-                    <h4>Registro de tu masa corporal</h4>
-                    <div class="row">
+                  <form class="mt-3" action="gimnasio.php" method="POST">
+                     <h4>Registro de tu masa corporal</h4>
+                     <div class="row">
                         <div class="col">
                         <input type="text" class="form-control" placeholder="altura" name="altura">
                         </div>
                         <div class="col">
                         <input type="number" class="form-control" placeholder="peso" name="peso">
                         </div>
-                    </div>
-                    <button type="submit" class="btn btn-info mt-3 btn-block" name="botonCalcular">CALCULAR</button>
-                </form>
-    <?php
-    if(isset($_POST["botonCalcular"])): ?>
-    <h4 class="text-danger text-center">
-        <?php
-        $altura=$_POST['altura'];
-        $peso=$_POST['peso'];
-        $superaltura = $altura * $altura;
-        $imc = $peso / $superaltura;
+                      </div>
+                      <button type="submit" class="btn btn-info mt-3 btn-block" name="botonCalcular">CALCULAR</button>
+                  </form>
+                  <?php
+                  if(isset($_POST["botonCalcular"])): ?>
+                  <h4 class="text-danger text-center">
+                  <?php
+                  $altura=$_POST['altura'];
+                  $peso=$_POST['peso'];
+                  $superaltura = $altura * $altura;
+                  $imc = $peso / $superaltura;
 
-        echo ("tu masa corporal es :  " .$imc);
+                  echo ("tu masa corporal es :  " .$imc);
 
-        $imc = $peso / $superaltura;
-        if ($imc < 18.5) {
-            echo "Usted tiene bajo peso" ;
-        } elseif ($imc >= 18.5 && $imc < 25) {
-            echo "usted tiene peso normal";
-        } elseif ($imc >= 25 && $imc < 27) {
-            echo "usted tiene sobrepeso grado 1 ";
-        } elseif ($imc >= 27 && $imc < 30) {
-            echo "usted tiene sobrepeso grado 2";
-        } elseif ($imc >= 30 && $imc < 35) {
-            echo "usted tiene obesidad grado 1";
-        } elseif ($imc >= 35 && $imc < 40) {
-            echo "usted tiene obesidad grado 2";
-        } elseif ($imc >= 40 && $imc < 50) {
-            echo "usted tiene obesidad tipo 3";
-        } elseif ($imc >= 50 ) {
-            echo "usted tienen obesidad tipo 4 (extrema)";
-        }else { 
-        }
-        ?>
-    </h4>
+                  $imc = $peso / $superaltura;
+                  if ($imc < 18.5) {
+                    echo "Usted tiene bajo peso" ;
+                  } elseif ($imc >= 18.5 && $imc < 25) {
+                   echo "usted tiene peso normal";
+                  } elseif ($imc >= 25 && $imc < 27) {
+                   echo "usted tiene sobrepeso grado 1 ";
+                  } elseif ($imc >= 27 && $imc < 30) {
+                   echo "usted tiene sobrepeso grado 2";
+                  } elseif ($imc >= 30 && $imc < 35) {
+                   echo "usted tiene obesidad grado 1";
+                  } elseif ($imc >= 35 && $imc < 40) {
+                   echo "usted tiene obesidad grado 2";
+                  } elseif ($imc >= 40 && $imc < 50) {
+                   echo "usted tiene obesidad tipo 3";
+                  } elseif ($imc >= 50 ) {
+                   echo "usted tienen obesidad tipo 4 (extrema)";
+                  }else { 
+                  }
+                  ?>
+                  </h4>
 
 
-    <?php endif ?>
-    
+                  <?php endif ?>
+                </div>
+            </div>
+        </div>
+    </main>   
 </body>
 </html>
